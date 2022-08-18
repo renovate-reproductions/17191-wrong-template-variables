6618cd22c5b83e8226a41a67f5f73c7da4c32d1e
Michael Kriese <michael.kriese@visualon.de>
2022-08-18 22:20:20 +0200

    chore: update test

--- test.tf
+++ test.tf
@@ -1,24 +1,5 @@
-resource "kubernetes_cron_job_v1" "demo" {
-  metadata {}
-  spec {
-    job_template {
-      metadata {}
-      spec {
-        template {
-          metadata {}
-          spec {
-            container {
-              name    = "kaniko"
-              image   = "gcr.io/kaniko-project/executor:v1.7.0@sha256:8504bde9a9a8c9c4e9a4fe659703d265697a36ff13607b7669a4caa4407baa52"
-            }
-            container {
-              name    = "node"
-              image   = "node:14"
-            }
-          }
-        }
-      }
-    }
-    schedule = ""
+terraform {
+  required_providers {
+    aws = ">= 2.7.0"
   }
 }
